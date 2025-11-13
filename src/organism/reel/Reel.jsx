c1da425cb9b6ab53bc966a10ce4918ceab8baa71
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-13 21:13:06 +0530

    Feat: Added code for fetching video from backend and populating reels component

---
+++ src/organism/reel/Reel.jsx
@@ -0,0 +1,41 @@
+import Video from "@/components/molecules/Video/video";
+import useFetchAllVideos from "@/hooks/apis/useFetchAllVideos";
+import { useEffect, useState } from "react";
+
+export const Reel = () => {
+
+  const [playing, setPlaying] = useState(1);
+  const [videoDownloaded, setVideoDownloaded] = useState([]);
+
+  const { isVideoFetching, isVideoError, isVideoFetched, videos} = useFetchAllVideos();
+
+  useEffect(() => {
+    if(isVideoFetched && videos)
+      setVideoDownloaded(videos);
+  }, [videos, isVideoFetched]);
+
+  if(isVideoFetching) {
+    return <div>Loading...</div>
+  }
+
+  if(isVideoError) {
+    return <div>Error fetching videos.</div>
+  }
+
+    return (
+        <div className='app'>
+        <div className='container'> 
+            {isVideoFetched && videoDownloaded.map((currentVideo) => (
+            <Video
+                key={currentVideo.id} 
+                id={currentVideo.id} 
+                url={currentVideo.url} 
+                caption={currentVideo.caption}
+                playing={playing}
+                setPlaying={setPlaying}
+            />
+            ))}
+        </div>
+        </div>
+    )
+}
