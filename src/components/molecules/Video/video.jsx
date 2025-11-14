e2e2872d8de242b7b615870484f6975bf87c9b82
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-14 16:56:36 +0530

    Feat: Added snap scroll with auto video playing

--- src/components/molecules/Video/video.jsx
+++ src/components/molecules/Video/video.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import "./video.css";
+import { ReelFooter } from "@/components/atoms/ReelFooter/ReelFooter";
 
 export default function Video({
     id,
@@ -24,13 +25,43 @@ export default function Video({
         if(playing !== id) {
             videoRef.current.pause();
         }
-    }, [playing]);
+    }, [playing, id]);
+
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver((entries) => {
+            
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    videoRef.current.play();
+                    setPlaying(id);
+                }
+            })
+
+        }, { threshold: 0.5 });
+
+        if(videoRef.current) {
+            observer.observe(videoRef.current);
+        }
+
+        return () => {
+            if(videoRef.current) {
+                observer.unobserve(videoRef.current);
+            }
+        }
+    }, [id, setPlaying]);
+
 
     return (
         <div className="video-wrapper">
             <video className="video-player" loop onClick={handleClick} ref={videoRef} src={url}>
 
             </video>
+
+            <div className="bottom">
+                <ReelFooter channel={"dilshaz_hussain832"} caption={caption} />
+            </div>
 
         </div>
     )
